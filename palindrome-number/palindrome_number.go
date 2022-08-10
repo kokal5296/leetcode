aff5576308f35b59da2e1225ad378b248e0721de
kokal5296 <tine.kokalj000@gmail.com>
2022-08-10 20:00:00 +0200

    solution for palindrome number

--- palindrome-number/palindrome_number.go
+++ palindrome-number/palindrome_number.go
@@ -1,16 +1,14 @@
 package main
 
-import (
-	"log"
-	"strconv"
-)
+import "strconv"
 
 //https://leetcode.com/problems/palindrome-number/
 
 func isPalindrome(x int) bool {
-	number := 41
-	number10 := 64
-	number1 := strconv.Itoa(number)
-	log.Println(number1 + "bob")
-
+	value := strconv.Itoa(x)
+	value2 := ""
+	for dolzina := len(value) - 1; 0 <= dolzina; dolzina-- {
+		value2 += string(value[dolzina])
+	}
+	return value == value2
 }
